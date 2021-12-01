d5a2125cf1af06d4d8cde166ba752dcbfd527908
mayaSamb <93666724+mayaSamb@users.noreply.github.com>
2021-12-01 00:21:41 +0000

    Add files via upload

---
+++ exofct1.php
@@ -0,0 +1,10 @@
+<?php
+function lemeilleurprof($x)
+{
+ $x= "Le prof de programmation Web";
+ return $x; 
+}
+
+echo "le meilleur prof nest pas " .lemeilleurprof($x) ;
+
+?>
---
+++ exofct2.php
@@ -0,0 +1,9 @@
+<?php
+function monArgument($abc)
+{
+    $abc = "a b c" ; 
+    return $abc;
+}
+ echo " le debut de lalphabet est : " .monArgument($abc);
+
+?>
